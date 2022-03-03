6425816fcecc6746a9a5f4393ae3c90866358a90
Pavel <pavelsuspicin@gmail.com>
2022-03-03 11:16:16 +0300

    030322

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function leapYear(year) {
+    return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0
+}
 
+console.log(leapYear(2016))
+console.log(leapYear(2000))
+console.log(leapYear(1700))
+console.log(leapYear(1800))
+console.log(leapYear(100))
